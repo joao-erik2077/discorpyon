475ddacdeb55c663ae7efdd973a17d6b28424934
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-04-04 18:20:56 -0300

    Testing some events

--- main.py
+++ main.py
@@ -17,4 +17,20 @@ async def on_message(message):
     if message.content.startswith('Hello'):
         await message.channel.send('Hello!')
 
+@client.event
+async def on_typing(channel, user, when):
+    await channel.send('Someone is typing...')
+
+@client.event
+async def on_message_delete(message):
+    await message.channel.send('Someone deleted a message')
+
+@client.event
+async def on_message_edit(before, after):
+    await before.channel.send('Someone edited a message')
+
+@client.event
+async def on_reaction_add(reaction, user):
+    await reaction.message.channel.send(f'Someone add a {reaction} reaction')
+
 client.run(os.getenv('TOKEN'))
